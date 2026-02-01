712020f1c1cd8336fc5874fbe5259f3559f800a8
sucharita4867 <sucharitasardar7@gmail.com>
2026-02-01 23:07:12 +0530

    added one line in contact

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact contact contact</div>;
+  return <div>Contact contact contact Contact contact contact</div>;
 };
 
 export default Contact;
